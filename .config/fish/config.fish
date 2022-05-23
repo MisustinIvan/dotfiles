7af1b4d37f746697d9d6a78dd9482a25cffd77d5
Ivan Mišustin <ivanmis596@gmail.com>
2022-05-23 18:59:59 +0200

    other dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+alias ls="lsd -a"
+alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	colorscript -r
+end
